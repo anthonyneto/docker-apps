43898a08b232ca46536e81869ee9c5a03c62b8f4
Chase Bolt <cbolt@denirostaff.com>
2015-09-01 17:06:45 -0700

    delete containers after they are created

--- test/cookbooks/docker-apps_test/recipes/default.rb
+++ test/cookbooks/docker-apps_test/recipes/default.rb
@@ -8,13 +8,44 @@ directory '/data/ssd'
 directory '/data/storage'
 
 include_recipe 'docker-apps::couchpotato'
+docker_container 'couchpotato' do
+  action :delete
+end
+
 include_recipe 'docker-apps::mumble'
+docker_container 'mumble' do
+  action :delete
+end
+
 include_recipe 'docker-apps::nzbget'
+docker_container 'nzbget' do
+  action :delete
+end
+
 include_recipe 'docker-apps::plexpy'
+docker_container 'plexpy' do
+  action :delete
+end
+
 include_recipe 'docker-apps::registry'
+docker_container 'registry' do
+  action :delete
+end
+
 include_recipe 'docker-apps::sonarr'
+docker_container 'sonarr' do
+  action :delete
+end
+
 include_recipe 'docker-apps::ventrilo'
+docker_container 'ventrilo' do
+  action :delete
+end
+
 include_recipe 'docker-apps::plex'
+docker_container 'plex' do
+  action :delete
+end
 
 # need to create a ssh key to pass into the usenet-tunnel
 execute 'create_ssh_key' do
@@ -29,6 +60,9 @@ ruby_block 'read_ssh_pub_key' do
 end
 
 include_recipe 'docker-apps::usenet-tunnel'
+docker_container 'usenet-tunnel' do
+  action :delete
+end
 
 # TODO: test ssh login process
 # execute 'test_ssh' do
